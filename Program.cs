ab0d77c094523c8d46ec3a5c2e66ab14224b403b
sintongsimon <sintongsimon.sihombing@gmail.com>
2020-01-03 10:40:40 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repotest4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
